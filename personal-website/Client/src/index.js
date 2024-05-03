0a1e9fc1da30809db3f368e5d87c11c0287d7a03
Bharat Pandey <bharatpandey@Bharats-Laptop.ht.home>
2024-05-03 16:50:43 -0400

    added index.html

--- personal-website/Client/src/index.js
+++ personal-website/Client/src/index.js
@@ -14,10 +14,6 @@ const router = createBrowserRouter ([
   path:"/",
    element:<App/>
 },
-{path:"Skills",
-element:<Skills/>,
-},
-
 
 ]) 
 const root = ReactDOM.createRoot(document.getElementById('root'));
